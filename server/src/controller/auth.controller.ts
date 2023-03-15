afa35b144858bed96a13d15683620096af796128
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-03-15 13:28:53 +0100

    add login function with access and refresh token

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {getRepository} from "typeorm";
 import {User} from "../entity/user.entity";
 import bcryptjs from 'bcryptjs';
+import {sign} from "jsonwebtoken";
 
 export const Register = async (req: Request, res: Response) => {
     const {name, email, password} = req.body;
@@ -13,4 +14,44 @@ export const Register = async (req: Request, res: Response) => {
     });
 
     res.send(user);
+}
+
+export const Login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+
+    const user = await getRepository(User).findOne({email});
+
+    if(!user) {
+        return res.status(400).send({
+            message: 'Invalid credentials'
+        })
+    }
+
+    if(!await bcryptjs.compare(password, user.password)) {
+        return res.status(400).send({
+            message: 'Invalid credentials'
+        })
+    }
+
+    const accessToken = sign({
+        id: user.id
+    }, "access_secret", {expiresIn: '30s'});
+
+    const refreshToken = sign({
+        id: user.id
+    }, "refresh_secret", {expiresIn: '1w'});
+
+    res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000 //1day
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000 //7days
+    });
+
+    res.send({
+        message: 'success'
+    });
 }
